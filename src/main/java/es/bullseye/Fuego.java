18e53fbae20913642afac519e5eebe531894028b
Luisma <perronegro49@gmail.com>
2022-06-26 17:04:18 +0200

    agrego lo que faltaba por persistir

--- src/main/java/es/bullseye/Fuego.java
+++ src/main/java/es/bullseye/Fuego.java
@@ -6,9 +6,18 @@ public class Fuego extends Arma implements FuegoInterfaz {
 	private double calibreEnMilimetros;
 	private String nombre;
 	private String sistemaAccion;
+	private int cargador;
 
 	// GETTERS Y SETTERS
 
+
+	public int getCargador() {
+		return cargador;
+	}
+
+	public void setCargador(int cargador) {
+		this.cargador = cargador;
+	}
 
 	@Override
 	public double getCalibreEnMilimetros() {
@@ -43,11 +52,12 @@ public class Fuego extends Arma implements FuegoInterfaz {
 		super();
 	}
 
-	public Fuego( double calibreEnMilimetros, String nombre, String sistemaAccion) {
+	public Fuego( double calibreEnMilimetros, String nombre, String sistemaAccion, int cargador) {
 		super();
 		this.nombre = nombre;
 		this.calibreEnMilimetros = calibreEnMilimetros;
 		this.sistemaAccion = sistemaAccion;
+		this.cargador= cargador;
 	}
 
 }
